7f2f30b35b343d98307bc6c0b520387c5930d596
Navin <Navi04399@gmail.com>
2024-03-23 18:11:59 +0530

    Iteration-4: with Bob TDD kata iteration
    Iteration-4: with Bob TDD kata iteration
    
    fix: refactor the code

--- com/kata/PrimeFactors.java
+++ com/kata/PrimeFactors.java
@@ -5,20 +5,28 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class PrimeFactors {
-
-    public static List<Integer> getFactorsOf(int n) {
+    public static List<Integer> getPrimeFactors(int n) {
         List<Integer> primeFactors = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                primeFactors.add(i);
-                n /= i;
-            }
+        for (int factor = 2; factor <= n; factor++) {
+            n = extractFactor(n, factor, primeFactors);
         }
         return primeFactors;
     }
 
+    private static int extractFactor(int n, int factor, List<Integer> primeFactors) {
+        while (isDivisible(n, factor)) {
+            primeFactors.add(factor);
+            n /= factor;
+        }
+        return n;
+    }
+
+    private static boolean isDivisible(int n, int fac) {
+        return n % fac == 0;
+    }
+
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(PrimeFactors.class.getName());
-        logger.info("This is Third Kata Iteration");
+        logger.info("Iteration 4 of Kata");
     }
 }
--- test/PrimeFactorsTest.java
+++ test/PrimeFactorsTest.java
@@ -1,55 +1,48 @@
 package test;
 
 import com.kata.PrimeFactors;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class PrimeFactorsTest {
+    private static void assertPrimeFactorsOf(int number, List<Integer> primeFactorsList) {
+        assertEquals(primeFactorsList, getPrimeFactors(number));
+    }
+
+    private static List<Integer> getPrimeFactors(int number) {
+        return PrimeFactors.getPrimeFactors(number);
+    }
+
     @Test
-    public void primeFactorsOf1Is(){
-        Assert.assertEquals(List.of(), PrimeFactors.getFactorsOf(1));
+    public void primeFactorsOf1() {
+        assertPrimeFactorsOf(1, List.of());
     }
 
     @Test
     public void primeFactorsOf2Is2() {
-        Assert.assertEquals(List.of(2), PrimeFactors.getFactorsOf(2));
+        assertPrimeFactorsOf(2, List.of(2));
     }
 
     @Test
     public void primeFactorsOf3Is3() {
-        Assert.assertEquals(List.of(3), PrimeFactors.getFactorsOf(3));
-    }
-
-    @Test
-    public void primeFactorsOf4IS2x2() {
-        Assert.assertEquals(List.of(2,2), PrimeFactors.getFactorsOf(4));
+        assertPrimeFactorsOf(3, List.of(3));
     }
 
     @Test
     public void primeFactorsOf6Is2x3() {
-        Assert.assertEquals(List.of(2,3), PrimeFactors.getFactorsOf(6));
+        assertPrimeFactorsOf(6, List.of(2, 3));
     }
 
     @Test
-    public void primeFactorsOf9Is3x3() {
-
-        Assert.assertEquals(List.of(3,3), PrimeFactors.getFactorsOf(9));
+    public void primeFactorsOf12Is2x2x3() {
+        assertPrimeFactorsOf(12, List.of(2, 2, 3));
     }
 
     @Test
-    public void primeFactorsOf25Is5x5() {
-        Assert.assertEquals(List.of(5,5), PrimeFactors.getFactorsOf(25));
-    }
-
-    @Test
-    public void primeFactorsOf30Is2x3x5() {
-        Assert.assertEquals(List.of(2,3,5), PrimeFactors.getFactorsOf(30));
-    }
-
-    @Test
-    public void primeFactorsOf2x3x5x7x3x2x5x7x71IsSame() {
-        Assert.assertEquals(List.of(2,2,3,3,5,5,7,7,71), PrimeFactors.getFactorsOf(2*3*5*7*3*2*5*7*71));
+    public void integration() {
+        assertPrimeFactorsOf(150, List.of(2, 3, 5, 5));
     }
 }
